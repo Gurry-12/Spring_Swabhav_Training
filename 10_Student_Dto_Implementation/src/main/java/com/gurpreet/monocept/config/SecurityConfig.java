28817245a01fb8c9d0ded4a94e873e3c99927997
Gurry-12 <96852785+Gurry-12@users.noreply.github.com>
2026-05-28 16:54:14 +0530

    feat: Implement Spring Security in Student app

---
+++ 10_Student_Dto_Implementation/src/main/java/com/gurpreet/monocept/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.gurpreet.monocept.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	// security filter chain
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+		httpSecurity.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET, "/api/students/**")
+						.hasAnyRole("Admin", "student").requestMatchers(HttpMethod.PUT, "/api/students/**")
+						.hasAnyRole("Admin", "student").requestMatchers(HttpMethod.PATCH, "/api/students/**")
+						.hasAnyRole("Admin", "student").requestMatchers(HttpMethod.POST, "/api/students/**")
+						.hasRole("Admin").requestMatchers(HttpMethod.DELETE, "/api/students/**").hasRole("Admin")
+						.anyRequest().authenticated())
+				.httpBasic(Customizer.withDefaults())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		return httpSecurity.build();
+	}
+	
+	@Bean 
+	UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) 
+	{
+		UserDetails admin = User.builder().username("Admin").password(passwordEncoder.encode("Admin123")).roles("Admin").build();
+		
+		UserDetails student = User.builder().username("student").password(passwordEncoder.encode("Student123")).roles("student").build();
+		
+		return new InMemoryUserDetailsManager(admin, student);
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
+}
